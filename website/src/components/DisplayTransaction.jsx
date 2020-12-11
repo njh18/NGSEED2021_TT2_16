beafb00c526787f32b6317ac9910e38011a037a5
Zhen Wei <zhennweii94@gmail.com>
2020-12-11 14:24:29 +0800

    WIP: call Service to get Transaction History

--- website/src/components/DisplayTransaction.jsx
+++ website/src/components/DisplayTransaction.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
-import AuthService from "../services/auth.service";
+import TransactionService from "../services/transaction.service";
 
 import { isEmail } from "validator";
 
@@ -17,54 +17,29 @@ const required = (value) => {
 };
 
 const DisplayTransaction = (props) => {
-  const form = useRef();
-  const checkBtn = useRef();
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState("");
+  const custID = localStorage.getItem("custID")
 
-  const onChangeUsername = (e) => {
-    const username = e.target.value;
-    setUsername(username);
-  };
+  useEffect(() => {
+    TransactionService.getTransaction(custID).then(
+      (response) => {
+        console.log(response);
+      
+      },
+      (error) => {
+        const _content =
+          (error.response && error.response.data) ||
+          error.message ||
+          error.toString();
+      }
+    );
+  }, []);
 
-  const onChangePassword = (e) => {
-    const password = e.target.value;
-    setPassword(password);
-  };
 
-  const handleLogin = (e) => {
-    e.preventDefault();
+  // To replace with data from api
+  const Payee = "Group 12";
 
-    setMessage("");
-    setLoading(true);
 
-    form.current.validateAll();
-
-    if (checkBtn.current.context._errors.length === 0) {
-      AuthService.login(username, password).then(
-        () => {
-          props.history.push("/profile");
-          window.location.reload();
-        },
-        (error) => {
-          const resMessage =
-            (error.response &&
-              error.response.data &&
-              error.response.data.message) ||
-            error.message ||
-            error.toString();
-
-          setLoading(false);
-          setMessage(resMessage);
-        }
-      );
-    } else {
-      setLoading(false);
-    }
-  };
 
   return (
     <div className="col-md-12">
@@ -79,7 +54,7 @@ const DisplayTransaction = (props) => {
         </thead>
          <tbody>
             <tr>
-                <td>  data.payeeID </td>
+                <td>  {Payee} </td>
                 <td>  data.Date </td>
                 <td>Amount</td>
                 <td>Category</td>
