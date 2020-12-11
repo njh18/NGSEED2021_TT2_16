269403887c7c4438328f7674a0f4a50c58c02657
njh18 <ngjunhao1996@gmail.com>
2020-12-11 12:14:17 +0800

    Website template

---
+++ website/src/services/auth-header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+const authHeader = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  if (user && user.accessToken) {
+    return { Authorization: "Bearer " + user.accessToken };
+  } else {
+    return {};
+  }
+};
+
+export default authHeader;
